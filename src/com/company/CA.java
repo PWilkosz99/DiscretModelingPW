1c2c8b7667efde5b4c70a41fb4ed01d6137fbf9a
PWilkoszAGH <pwilkosz99@gmail.com>
2021-10-27 10:50:31 +0200

    Main logic of CA

--- src/com/company/CA.java
+++ src/com/company/CA.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class CA {
 
-    public static int convertToDecimal(Character s0, Character s1, Character s2){
-        return Character.getNumericValue(s0)+(Character.getNumericValue(s1)*2)+(Character.getNumericValue(s2)*4);
+    public static int convertToDecimal(Integer s0, Integer s1, Integer s2){
+        return s0+(s1*2)+(s2*4);
     }
 
     public static char[] convertToBinary(int x){
@@ -19,8 +19,29 @@ public class CA {
         return bs.toCharArray();
     }
 
+    public static Boolean checkNext(int upperValue, char[] rule){
+        return rule[upperValue] == '1';
+    }
+
+    public static void CA(int[][]Table, int ruleNumber){
+        int length, width;
+        length=10;
+        width=5; //to be changed
+        char[] rule = convertToBinary(ruleNumber);
+        for (int i = 1; i <length; i++) {
+            for (int j = 0; j < width-1; j++) {
+                var d = convertToDecimal(Table[i-1][j],Table[i][j],Table[i+1][j]);
+                if(checkNext(d,rule)){
+                    Table[i][j+1]=1;
+                }
+            }
+        }
+    }
+
+    //TODO: drawing
+
     public static void main(String[] args) throws IOException {
-        System.out.println(convertToDecimal('1','1','1'));
-        System.out.println(convertToBinary(128)[0]);
+        System.out.println(convertToDecimal(1,1,1));
+        System.out.println(convertToBinary(128));
     }
 }
