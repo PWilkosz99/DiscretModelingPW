2f54e840aee49c851b5efd20c6df3d0f53ce6768
PWilkosz99 <pwilkosz99@gmail.com>
2021-11-11 23:01:50 +0100

    Wind direction analysis

--- src/com/company/ForestFireCA.java
+++ src/com/company/ForestFireCA.java
@@ -3,8 +3,8 @@ package com.company;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 
 public class ForestFireCA {
@@ -16,11 +16,20 @@ public class ForestFireCA {
     private double propagationRatio; //percent
     private int ii;
     private Byte[][] burningTime;
+    private Direction direction;
 
     private enum Tree {
         DEAD,
         BURNING,
-        LIFE
+        LIFE,
+        EXCLUDED
+    }
+
+    public enum Direction {
+        North,
+        South,
+        West,
+        East
     }
 
     ForestFireCA(int x, int y) {
@@ -29,16 +38,18 @@ public class ForestFireCA {
         Forest = new Tree[x][y];
     }
 
-    ForestFireCA(int x, int y, double TP, double FP, double P) {
+    ForestFireCA(int x, int y, double TP, double FP, double P, Direction dir) {
         this(x, y);
         newTreeProbability = TP;
         newFireProbability = FP;
         propagationRatio = P;
         burningTime = new Byte[x][y];
+        direction=dir;
     }
 
     public void StartSimulation(int iter) throws IOException {
         fillArrayByZero();
+        getExclusions();
         afforestation(25);
         ii = 0;
         print();
@@ -53,16 +64,35 @@ public class ForestFireCA {
     }
 
     public void fillArrayByZero() {
-        for (int i = 0; i < y; i++) {
-            for (int j = 0; j < x; j++) {
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
                 Forest[i][j] = Tree.DEAD;
-                burningTime[i][j]=-1;
+                burningTime[i][j] = -1;
             }
         }
     }
 
     private int getRandom() {
         return (int) (Math.random() * 1000); //0-999
+    }
+
+    private void getExclusions() throws FileNotFoundException {
+        Scanner sc = new Scanner(new BufferedReader(new FileReader("Mapa_MD_no_terrain_low_res_Gray.txt")));
+        int rows = 330;
+        int columns = 600;
+        int[][] bitArray = new int[rows][columns];
+        while (sc.hasNextLine()) {
+            for (int i = 0; i < bitArray.length; i++) {
+                String[] line = sc.nextLine().trim().split("\t");
+                for (int j = 0; j < line.length; j++) {
+                    if (Integer.parseInt(line[j]) < 165) {
+                        Forest[j][i] = Tree.EXCLUDED;
+                    } else {
+                        Forest[j][i] = Tree.DEAD;
+                    }
+                }
+            }
+        }
     }
 
     public void afforestation(double prob) {
@@ -87,7 +117,7 @@ public class ForestFireCA {
                 if (Forest[i][j] == Tree.LIFE) {
                     if (getRandom() < 10 * newFireProbability) {
                         Forest[i][j] = Tree.BURNING;
-                        burningTime[i][j]=0;
+                        burningTime[i][j] = 0;
                     }
                 }
             }
@@ -102,28 +132,67 @@ public class ForestFireCA {
 
     private void neighborImmolation() {
         Tree[][] tmp = new Tree[x][y];
-        int burningNeighboursCount;
+        int N, S, W, E;
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
                 if (Forest[i][j] == Tree.LIFE) {
-                    burningNeighboursCount = 0;
-                    if (checkStateSafe(i - 1, j - 1)) burningNeighboursCount++;
-                    if (checkStateSafe(i, j - 1)) burningNeighboursCount++;
-                    if (checkStateSafe(i + 1, j - 1)) burningNeighboursCount++;
-                    if (checkStateSafe(i - 1, j)) burningNeighboursCount++;
-                    if (checkStateSafe(i + 1, j)) burningNeighboursCount++;
-                    if (checkStateSafe(i - 1, j + 1)) burningNeighboursCount++;
-                    if (checkStateSafe(i, j + 1)) burningNeighboursCount++;
-                    if (checkStateSafe(i + 1, j + 1)) burningNeighboursCount++;
-
-                    if (getRandom() < burningNeighboursCount * propagationRatio) {
-                        tmp[i][j] = Tree.BURNING;
-                        burningTime[i][j]=0;
-                    }else{
-                        tmp[i][j]=Forest[i][j];
+                    N = S = W = E = 0;
+                    if (checkStateSafe(i - 1, j - 1)) {
+                        N++;
+                        W++;
                     }
-                }else{
-                    tmp[i][j]=Forest[i][j];
+                    if (checkStateSafe(i, j - 1)) {
+                        N++;
+                    }
+                    if (checkStateSafe(i + 1, j - 1)) {
+                        W++;
+                        E++;
+                    }
+                    if (checkStateSafe(i - 1, j)) {
+                        W++;
+                    }
+                    if (checkStateSafe(i + 1, j)) {
+                        E++;
+                    }
+                    if (checkStateSafe(i - 1, j + 1)) {
+                        W++;
+                        S++;
+                    }
+                    if (checkStateSafe(i, j + 1)) {
+                        S++;
+                    }
+                    if (checkStateSafe(i + 1, j + 1)) {
+                        S++;
+                        E++;
+                    }
+
+                    switch (direction){
+                        case North:
+                            N*=1.5;
+                            S*=0.5;
+                            break;
+                        case South:
+                            N*=0.5;
+                            S*=1.5;
+                            break;
+                        case West:
+                            W*=1.5;
+                            E*=0.5;
+                            break;
+                        case East:
+                            W*=0.5;
+                            E*=1.5;
+                            break;
+                    }
+
+                    if (getRandom() < (N+S+W+E) * propagationRatio * 10) {//without wind burningNeighboursCount * propagationRatio * 10
+                        tmp[i][j] = Tree.BURNING;
+                        burningTime[i][j] = 0;
+                    } else {
+                        tmp[i][j] = Forest[i][j];
+                    }
+                } else {
+                    tmp[i][j] = Forest[i][j];
                 }
             }
         }
@@ -135,7 +204,7 @@ public class ForestFireCA {
             for (int j = 0; j < y; j++) {
                 if (burningTime[i][j] > 3) {
                     Forest[i][j] = Tree.DEAD;
-                    burningTime[i][j]=-1;
+                    burningTime[i][j] = -1;
                 } else if (burningTime[i][j] >= 0) {
                     burningTime[i][j]++;
                 }
@@ -152,6 +221,8 @@ public class ForestFireCA {
                     g.setColor(new Color(5, 229, 5));
                 } else if (Forest[x - (i / 5)][y - (j / 5)] == Tree.BURNING) {
                     g.setColor(new Color(255, 0, 0));
+                } else if (Forest[x - (i / 5)][y - (j / 5)] == Tree.EXCLUDED) {
+                    g.setColor(new Color(16, 140, 187));
                 } else {
                     g.setColor(new Color(0, 0, 0));
                 }
@@ -163,7 +234,7 @@ public class ForestFireCA {
     }
 
     public static void main(String[] args) throws IOException {
-        ForestFireCA forest = new ForestFireCA(50, 50, 2.5, 0.5, 1000);
+        ForestFireCA forest = new ForestFireCA(600, 330, 2.5, 0.5, 100, Direction.North);
         forest.StartSimulation(20);
         forest.print();
     }
